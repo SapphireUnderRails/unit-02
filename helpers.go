eff67fd6e465892b8618079e08e9c3fa4e5393cd
Sapphire <quinton.oglesby@icloud.com>
2022-08-03 18:41:33 -0500

    Update helpers.go

--- helpers.go
+++ helpers.go
@@ -1,12 +1,18 @@
 package main
 
-// Function to check if a key with a type of string is in a map.
-func stringInKeys(key string, m map[string]int64) bool {
-	for k := range m {
-		if k == key {
-			return true
-		}
-	}
+// // Function to iterate over the array of users on cooldown
+// // and to return a bool whether or not is in the list and
+// // the index of the desired user if that user is in the array.
+// func indexOfString(desiredKey string, arr []UserCooldown) (bool, int64) {
+// 	// Iterate over the array.
+// 	for index, key := range arr {
+// 		// Check if the desired key matches the key at the current index.
+// 		if key.userID == desiredKey {
+// 			return true, int64(index)
+// 		}
+// 	}
 
-	return false
-}
+// 	// We couldn't find a key that matches so we return -1
+// 	// to signal that we could not find the desired key.
+// 	return false, -1
+// }
