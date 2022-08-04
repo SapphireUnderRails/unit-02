68acd1d0fcf02e7f7ee58ec356ceb618c4ba8dd2
Sapphire <quinton.oglesby@icloud.com>
2022-08-04 18:31:08 -0500

    Update helpers.go

--- helpers.go
+++ helpers.go
@@ -1,22 +1,5 @@
 package main
 
-// // Function to iterate over the array of users on cooldown
-// // and to return a bool whether or not is in the list and
-// // the index of the desired user if that user is in the array.
-// func indexOfString(desiredKey string, arr []UserCooldown) (bool, int64) {
-// 	// Iterate over the array.
-// 	for index, key := range arr {
-// 		// Check if the desired key matches the key at the current index.
-// 		if key.userID == desiredKey {
-// 			return true, int64(index)
-// 		}
-// 	}
-
-// 	// We couldn't find a key that matches so we return -1
-// 	// to signal that we could not find the desired key.
-// 	return false, -1
-// }
-
 var (
 	Reset  = "\033[0m"
 	Bold   = "\033[1m"
