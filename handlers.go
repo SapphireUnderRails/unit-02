e0928999ec982630ec5e1bdd23d53ea3c3c9e0b3
Sapphire <quinton.oglesby@icloud.com>
2022-08-08 17:46:00 -0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -358,7 +358,8 @@ var commandHandlers = map[string]func(session *discordgo.Session, interaction *d
 			if len(interaction.ApplicationCommandData().Options) == 0 {
 				query = `SELECT card_id, card_image FROM cards WHERE evolution = 1 ORDER BY RAND() LIMIT 1;`
 			} else {
-				query = fmt.Sprintf(`SELECT card_id, card_image FROM cards WHERE evolution = 1 AND character_name = "%v" ORDER BY RAND() LIMIT 1;`, strings.Title(interaction.ApplicationCommandData().Options[0].StringValue()))
+				query = fmt.Sprintf(`SELECT card_id, card_image FROM cards WHERE evolution = 1 AND character_name = "%v" ORDER BY RAND() LIMIT 1;`,
+					strings.Title(interaction.ApplicationCommandData().Options[0].StringValue()))
 			}
 			err = db.QueryRow(query).Scan(&drawnCardID, &cardImage)
 			if err != nil && err != sql.ErrNoRows {
@@ -372,7 +373,7 @@ var commandHandlers = map[string]func(session *discordgo.Session, interaction *d
 					Type: discordgo.InteractionResponseChannelMessageWithSource,
 					Data: &discordgo.InteractionResponseData{
 						//Content: fmt.Sprintf("Successfully added %v to your collection. You can rename this card at anytime by using `/rename [original_name] [new_name]", drawnCardID),
-						Content: "Are you sure you spelled that character's name right?",
+						Content: "I couldn't find that character pool. Are you sure you spelled that character's name right?",
 					},
 				})
 
@@ -465,7 +466,7 @@ var commandHandlers = map[string]func(session *discordgo.Session, interaction *d
 						Type: discordgo.InteractionResponseChannelMessageWithSource,
 						Data: &discordgo.InteractionResponseData{
 							//Content: fmt.Sprintf("Successfully added %v to your collection. You can rename this card at anytime by using `/rename [original_name] [new_name]", drawnCardID),
-							Content: fmt.Sprintf("Whoah there! You've already maxed out your %v, I've refunded your draw. Go ahead and try again!", customName),
+							Content: fmt.Sprintf("Whoah there! You've already maxed out your %v, I've refunded half your draw. Go ahead and try again!", customName),
 							Embeds:  embeds,
 						},
 					})
@@ -513,7 +514,7 @@ var commandHandlers = map[string]func(session *discordgo.Session, interaction *d
 						Type: discordgo.InteractionResponseChannelMessageWithSource,
 						Data: &discordgo.InteractionResponseData{
 							//Content: fmt.Sprintf("Successfully added %v to your collection. You can rename this card at anytime by using `/rename [original_name] [new_name]", drawnCardID),
-							Content: fmt.Sprintf("Check it out! You've leveled up your %v!", customName),
+							Content: fmt.Sprintf("Check it out! You've evolved your %v!", customName),
 							Embeds:  embeds,
 						},
 					})
@@ -574,6 +575,287 @@ var commandHandlers = map[string]func(session *discordgo.Session, interaction *d
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
 				Data: &discordgo.InteractionResponseData{
 					Content: "You do not have enough credits to draw a card.",
+				},
+			})
+
+			return
+		}
+	},
+	"ten_pull": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
+		var credits int64
+		var drawnCardID string
+		var evolution int8
+		var customName string
+		var cardImage string
+
+		var id int64
+		authorID := interaction.Member.User.ID
+
+		// Perform a single row query to make sure the user is registered.
+		query := fmt.Sprintf(`SELECT id FROM users_registration WHERE user_id = %s;`, authorID)
+		err := db.QueryRow(query).Scan(&id)
+		if err != nil && err != sql.ErrNoRows {
+			log.Printf("%vERROR%v - COULD NOT RETRIEVE USER FROM REGISTRATION DATABASE:\n\t%v", Red, Reset, err)
+			return
+		}
+
+		if err == sql.ErrNoRows {
+			// https: //pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
+			session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Hey! You aren't registered to play yet! Remember to use the command `/register` before trying to play!",
+				},
+			})
+			return
+		}
+
+		// Snagging the amount of credits so that they can be checked against.
+		query = fmt.Sprintf(`SELECT credits FROM users_registration WHERE user_id = %v;`, authorID)
+		err = db.QueryRow(query).Scan(&credits)
+		if err != nil {
+			log.Printf("%vERROR%v - COULD NOT GET CREDITS OF USER IN DATABASE: %v", Red, Reset, err)
+			return
+		}
+
+		if credits >= int64(1500) {
+			// Updating the amount of credits in the database for the user.
+			query = fmt.Sprintf(`UPDATE users_registration SET credits = %v WHERE user_id = %v;`, credits-int64(1500), authorID)
+			result, err := db.Exec(query)
+			if err != nil {
+				log.Printf("%vERROR%v - COULD NOT UPDATE CREDITS IN DATABASE: %v", Red, Reset, err)
+				return
+			}
+			log.Printf("%vSUCCESS%v - UPDATED USER CREDITS: %v", Green, Reset, result)
+
+			// https: //pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
+			session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: fmt.Sprintf("Now drawing ten cards for %v#%v...",
+						interaction.Member.User.Username, interaction.Member.User.Discriminator),
+				},
+			})
+
+			for i := 0; i < 10; i++ {
+				// Performing a single row query to draw a card with optional character.
+				var query string
+				if len(interaction.ApplicationCommandData().Options) == 0 {
+					query = `SELECT card_id, card_image FROM cards WHERE evolution = 1 ORDER BY RAND() LIMIT 1;`
+				} else {
+					query = fmt.Sprintf(`SELECT card_id, card_image FROM cards WHERE evolution = 1 AND character_name = "%v" ORDER BY RAND() LIMIT 1;`,
+						strings.Title(interaction.ApplicationCommandData().Options[0].StringValue()))
+				}
+				err = db.QueryRow(query).Scan(&drawnCardID, &cardImage)
+				if err != nil && err != sql.ErrNoRows {
+					log.Printf("%vERROR%v - COULD NOT GET A CARD TO DRAW FROM DATABASE: %v", Red, Reset, err)
+					return
+				}
+
+				if err == sql.ErrNoRows {
+					// https: //pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
+					session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+						Type: discordgo.InteractionResponseChannelMessageWithSource,
+						Data: &discordgo.InteractionResponseData{
+							//Content: fmt.Sprintf("Successfully added %v to your collection. You can rename this card at anytime by using `/rename [original_name] [new_name]", drawnCardID),
+							Content: "I couldn't find that character pool. Are you sure you spelled that character's name right?",
+						},
+					})
+
+					return
+				}
+
+				// Performing a single row query to check if the user already has the card in their collection.
+				query = fmt.Sprintf(`SELECT card_id, evolution FROM users_collection WHERE card_id = "%v" AND user_id = %v;`, drawnCardID, authorID)
+				err = db.QueryRow(query).Scan(&drawnCardID, &evolution)
+				if err != nil && err != sql.ErrNoRows {
+					log.Printf("%vERROR%v - COULD NOT QUERY USER COLLECTION IN DATABASE: %v", Red, Reset, err)
+					return
+				}
+
+				if err == sql.ErrNoRows {
+					// The user does not have the card in their collection.
+					// Query to insert the card into the user's collection.
+					query = fmt.Sprintf(`INSERT INTO users_collection(user_id, card_id, evolution, custom_name) VALUES("%v", "%v", "%v", "%v");`,
+						authorID, drawnCardID, 1, drawnCardID)
+
+					// Executing that query.
+					result, err := db.Exec(query)
+					if err != nil {
+						log.Printf("%vERROR%v - COULD NOT PLACE CARD IN USER COLLECTION DATABASE: %v", Red, Reset, err)
+						return
+					}
+					log.Printf("%vSUCCESS%v - PLACED CARD INTO USER COLLECTION DATABASE: %v", Green, Reset, result)
+
+					// Constructing an embed to hold the card image.
+					image := discordgo.MessageEmbedImage{
+						URL: cardImage,
+					}
+					embeds := []*discordgo.MessageEmbed{
+						{
+							Image: &image,
+						},
+					}
+
+					webhook := discordgo.WebhookParams{
+						Embeds:  embeds,
+						Content: fmt.Sprintf("(%d/10) You've drawn %v! I've added it to your collection.", i+1, drawnCardID),
+					}
+
+					// Informing the user that they have maxxed out the level on the card.
+					// https: //pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
+					session.FollowupMessageCreate(interaction.Interaction, true, &webhook)
+				} else {
+					// The user does have this card, the only question is what level do they have?
+					if evolution == 3 {
+						// If the evolution level is the max level, then we need to refund the user for this draw.
+						// Updating the amount of credits in the database for the user.
+						query = fmt.Sprintf(`UPDATE users_registration SET credits = %v WHERE user_id = %v;`, credits+int64(100), authorID)
+						result, err := db.Exec(query)
+						if err != nil {
+							log.Printf("%vERROR%v - COULD NOT UPDATE CREDITS IN DATABASE: %v", Red, Reset, err)
+							return
+						}
+						log.Printf("%vSUCCESS%v - UPDATED USER CREDITS: %v", Green, Reset, result)
+
+						// Creating a query to retrieve the image of the maximum level card.
+						query = fmt.Sprintf(`SELECT card_image FROM cards where card_id = "%v" AND evolution = %v`, drawnCardID, evolution)
+						err = db.QueryRow(query).Scan(&cardImage)
+						if err != nil {
+							log.Printf("%vERROR%v - COULD QUERY CARDS IN DATABASE: %v", Red, Reset, err)
+							return
+						}
+
+						// Creating a query to retrieve the user's custom name of the card.
+						query = fmt.Sprintf(`SELECT custom_name FROM users_collection WHERE user_id = %v AND card_id = "%v"`, authorID, drawnCardID)
+						err = db.QueryRow(query).Scan(&customName)
+						if err != nil {
+							log.Printf("%vERROR%v - COULD NOT GET CUSTOM NAME FROM DATABASE: %v", Red, Reset, err)
+							return
+						}
+
+						// Constructing an embed to hold the card image.
+						image := discordgo.MessageEmbedImage{
+							URL: cardImage,
+						}
+						embeds := []*discordgo.MessageEmbed{
+							{
+								Image: &image,
+							},
+						}
+
+						webhook := discordgo.WebhookParams{
+							Embeds:  embeds,
+							Content: fmt.Sprintf("(%d/10) Whoah there! You've already maxxed out your %v! I've given you 100 credits back.", i+1, customName),
+						}
+
+						// Informing the user that they have maxxed out the level on the card.
+						// https: //pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
+						session.FollowupMessageCreate(interaction.Interaction, true, &webhook)
+					} else if evolution == 2 {
+						// If the evolution level is 2, then we need to evolve the card to level 3.
+
+						// Updating the evolution level in the database of the user's card.
+						query = fmt.Sprintf(`UPDATE users_collection SET evolution = 3 WHERE user_id = %v AND card_id = "%v";`, authorID, drawnCardID)
+						result, err := db.Exec(query)
+						if err != nil {
+							log.Printf("%vERROR%v - COULD NOT UPDATE USER EVOLUTION IN DATABASE: %v", Red, Reset, err)
+							return
+						}
+						log.Printf("%vSUCCESS%v - UPDATED USER EVOLUTION: %v", Green, Reset, result)
+
+						// Creating a query to retrieve the image of the maximum level card.
+						query = fmt.Sprintf(`SELECT card_image FROM cards WHERE card_id = "%v" AND evolution = 3;`, drawnCardID)
+						err = db.QueryRow(query).Scan(&cardImage)
+						if err != nil {
+							log.Printf("%vERROR%v - COULD QUERY CARDS IN DATABASE: %v", Red, Reset, err)
+							return
+						}
+
+						// Creating a query to retrieve the user's custom name of the card.
+						query = fmt.Sprintf(`SELECT custom_name FROM users_collection WHERE user_id = %v AND card_id = "%v";`, authorID, drawnCardID)
+						err = db.QueryRow(query).Scan(&customName)
+						if err != nil {
+							log.Printf("%vERROR%v - COULD NOT GET CUSTOM NAME FROM DATABASE: %v", Red, Reset, err)
+							return
+						}
+						// Constructing an embed to hold the card image.
+						image := discordgo.MessageEmbedImage{
+							URL: cardImage,
+						}
+						embeds := []*discordgo.MessageEmbed{
+							{
+								Image: &image,
+							},
+						}
+
+						webhook := discordgo.WebhookParams{
+							Embeds:  embeds,
+							Content: fmt.Sprintf("(%d/10) Check it out! You've evolved your %v!", i+1, customName),
+						}
+
+						// Informing the user that they have maxxed out the level on the card.
+						// https: //pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
+						session.FollowupMessageCreate(interaction.Interaction, true, &webhook)
+					} else if evolution == 1 {
+						// If the evolution level is 1, then we need to evolve the card to level 2.
+
+						// Updating the evolution level in the database of the user's card.
+						query = fmt.Sprintf(`UPDATE users_collection SET evolution = 2 WHERE user_id = %v AND card_id = "%v";`, authorID, drawnCardID)
+						result, err := db.Exec(query)
+						if err != nil {
+							log.Printf("%vERROR%v - COULD NOT UPDATE USER EVOLUTION IN DATABASE: %v", Red, Reset, err)
+							return
+						}
+						log.Printf("%vSUCCESS%v - UPDATED USER EVOLUTION: %v", Green, Reset, result)
+
+						// Creating a query to retrieve the image of the maximum level card.
+						query = fmt.Sprintf(`SELECT card_image FROM cards where card_id = "%v" AND evolution = 2;`, drawnCardID)
+						err = db.QueryRow(query).Scan(&cardImage)
+						if err != nil {
+							log.Printf("%vERROR%v - COULD QUERY CARDS IN DATABASE: %v", Red, Reset, err)
+							return
+						}
+
+						// Creating a query to retrieve the user's custom name of the card.
+						query = fmt.Sprintf(`SELECT custom_name FROM users_collection WHERE user_id = %v AND card_id = "%v";`, authorID, drawnCardID)
+						err = db.QueryRow(query).Scan(&customName)
+						if err != nil {
+							log.Printf("%vERROR%v - COULD NOT GET CUSTOM NAME FROM DATABASE: %v", Red, Reset, err)
+							return
+						}
+
+						// Constructing an embed to hold the card image.
+						image := discordgo.MessageEmbedImage{
+							URL: cardImage,
+						}
+						embeds := []*discordgo.MessageEmbed{
+							{
+								Image: &image,
+							},
+						}
+
+						webhook := discordgo.WebhookParams{
+							Embeds:  embeds,
+							Content: fmt.Sprintf("(%d/10) Check it out! You've evolved your %v!", i+1, customName),
+						}
+
+						// Informing the user that they have maxxed out the level on the card.
+						// https: //pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
+						session.FollowupMessageCreate(interaction.Interaction, true, &webhook)
+					}
+				}
+				// Sleeping for a couple seconds to let the user see the card.
+				time.Sleep(time.Second)
+				time.Sleep(time.Second)
+			}
+		} else {
+			// The user does not have enough credits...
+			// https: //pkg.go.dev/github.com/bwmarrin/discordgo#Session.InteractionRespond
+			session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "You do not have enough credits to draw ten cards.",
 				},
 			})
 
